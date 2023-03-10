af6fd49d6b263655dbb94d927915f96f42b48dc9
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2023-03-09 21:25:02 -0300

    configs added

--- lua/danistry/settings.lua
+++ lua/danistry/settings.lua
@@ -1,7 +1,34 @@
-
 --configs personales
 vim.g.mapleader = " "
 vim.opt.backup = false
 vim.opt.errorbells = false
 vim.opt.swapfile = false
 vim.opt.signcolumn = 'yes'
+vim.cmd("autocmd!")
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.wo.number = false
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
+vim.opt.wildignore:append { '*/node_modules/*' }
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append { 'r' }
