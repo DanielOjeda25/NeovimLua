373f6b49a4beff6884f1ff4bde662ec0eb736211
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2023-03-09 21:24:44 -0300

    mini changes

--- lua/danistry/remap.lua
+++ lua/danistry/remap.lua
@@ -1,8 +1,30 @@
 local opts = { noremap = true, silent = true }
 
+--salir del insert mode
 vim.keymap.set("i", "jj", "<ESC>", opts)
-vim.keymap.set("n", "<leader>V", ":vsplit<CR>", opts)
 
 -- Clear search highlight
 vim.keymap.set("n", "<esc>", ":noh<return><esc>", opts)
+
+--split
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+
+--salir del insert modekeymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('', 'sh', '<C-w>h')
+vim.keymap.set('', 'sk', '<C-w>k')
+vim.keymap.set('', 'sj', '<C-w>j')
+vim.keymap.set('', 'sl', '<C-w>l')
+
+
+	-- Resize window
+--vim.keymap.set('n', '<C-w><left>', '<C-w><')
+--vim.keymap.set('n', '<C-w><right>', '<C-w>>')
+--vim.keymap.set('n', '<C-w><up>', '<C-w>+')
+--vim.keymap.set('n', '<C-w><down>', '<C-w>-')
+
+
+
 
